25eca28124a873d25725dc02f86d1332909f5ad3
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2023-01-22 18:55:04 -0600

    WebResponse added

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
+#[derive(Debug)]
 pub struct WebError {
     pub code: i16,
     pub description: String,
 }
 
+#[derive(Debug)]
+
 pub struct Metadata {
     pub version: i16,
     pub data_type: String,
+}
+
+#[derive(Debug)]
+pub struct WebResponse<T> {
+    pub success: bool,
+    pub metadata: Metadata,
+    pub error: Option<WebError>,
+    pub data: Option<Box<T>>,
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,6 +6,7 @@ fn test_web_error_struct() {
         code: 500,
         description: "Internal Server Error".to_string(),
     };
+
     assert_eq!(error.code, 500);
     assert_eq!(error.description, "Internal Server Error".to_string());
 }
@@ -16,6 +17,32 @@ fn test_metadata_struct() {
         version: 1,
         data_type: "Metadata".to_string(),
     };
+
     assert_eq!(meta.version, 1);
     assert_eq!(meta.data_type, "Metadata".to_string());
+}
+
+#[test]
+fn test_web_response_struct_string_success() {
+    let response: WebResponse<String> = WebResponse::<String> {
+        success: true,
+        metadata: Metadata {
+            version: 1,
+            data_type: "Success".to_string(),
+        },
+        error: None,
+        data: Some(Box::new(String::from("OK"))),
+    };
+
+    assert_eq!(response.success, true);
+    assert_eq!(response.metadata.version, 1);
+    assert_eq!(response.metadata.data_type, "Success".to_string());
+    assert!(response.error.is_none());
+
+    let value: Option<Box<String>> = response.data;
+
+    assert_eq!(
+        value.unwrap_or_else(|| Box::new(String::from(""))),
+        Box::new(String::from("OK"))
+    );
 }
